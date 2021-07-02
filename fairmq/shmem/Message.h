33baa4a08250562cfbfd97925ec160b4e0ab7f16
Alexey Rybalchenko <alexryba@gmail.com>
2021-07-02 13:26:58 +0200

    shm: reimplement alignment

--- fairmq/shmem/Message.h
+++ fairmq/shmem/Message.h
@@ -187,7 +187,8 @@ class Message final : public fair::mq::Message
             if (fMeta.fRegionId == 0) {
                 if (fMeta.fSize > 0) {
                     fManager.GetSegment(fMeta.fSegmentId);
-                    fLocalPtr = reinterpret_cast<char*>(fManager.GetAddressFromHandle(fMeta.fHandle, fMeta.fSegmentId));
+                    ShmPtr shmPtr(reinterpret_cast<char*>(fManager.GetAddressFromHandle(fMeta.fHandle, fMeta.fSegmentId)));
+                    fLocalPtr = shmPtr.GetUserPtr();
                 } else {
                     fLocalPtr = nullptr;
                 }
@@ -202,7 +203,7 @@ class Message final : public fair::mq::Message
             }
         }
 
-        return fLocalPtr;
+        return static_cast<void*>(fLocalPtr);
     }
 
     size_t GetSize() const override { return fMeta.fSize; }
@@ -217,7 +218,8 @@ class Message final : public fair::mq::Message
         } else if (newSize <= fMeta.fSize) {
             try {
                 try {
-                    fLocalPtr = fManager.ShrinkInPlace(newSize, fLocalPtr, fMeta.fSegmentId);
+                    ShmPtr shmPtr(fManager.ShrinkInPlace(newSize, static_cast<char*>(fManager.GetAddressFromHandle(fMeta.fHandle, fMeta.fSegmentId)), fMeta.fSegmentId));
+                    fLocalPtr = shmPtr.GetUserPtr();
                     fMeta.fSize = newSize;
                     return true;
                 } catch (boost::interprocess::bad_alloc& e) {
@@ -225,12 +227,13 @@ class Message final : public fair::mq::Message
                     // unused size >= 1000000 bytes: reallocate fully
                     // unused size < 1000000 bytes: simply reset the size and keep the rest of the buffer until message destruction
                     if (fMeta.fSize - newSize >= 1000000) {
-                        char* newPtr = fManager.Allocate(newSize, fAlignment);
-                        if (newPtr) {
-                            std::memcpy(newPtr, fLocalPtr, newSize);
+                        ShmPtr shmPtr = fManager.Allocate(newSize, fAlignment);
+                        if (shmPtr.GetRealPtr()) {
+                            char* userPtr = shmPtr.GetUserPtr();
+                            std::memcpy(userPtr, fLocalPtr, newSize);
                             fManager.Deallocate(fMeta.fHandle, fMeta.fSegmentId);
-                            fLocalPtr = newPtr;
-                            fMeta.fHandle = fManager.GetHandleFromAddress(fLocalPtr, fMeta.fSegmentId);
+                            fLocalPtr = userPtr;
+                            fMeta.fHandle = fManager.GetHandleFromAddress(shmPtr.GetRealPtr(), fMeta.fSegmentId);
                         } else {
                             LOG(debug) << "could not set used size: " << e.what();
                             return false;
@@ -288,11 +291,12 @@ class Message final : public fair::mq::Message
 
     char* InitializeChunk(const size_t size, size_t alignment = 0)
     {
-        fLocalPtr = fManager.Allocate(size, alignment);
-        if (fLocalPtr) {
-            fMeta.fHandle = fManager.GetHandleFromAddress(fLocalPtr, fMeta.fSegmentId);
+        ShmPtr shmPtr = fManager.Allocate(size, alignment);
+        if (shmPtr.GetRealPtr()) {
+            fMeta.fHandle = fManager.GetHandleFromAddress(shmPtr.GetRealPtr(), fMeta.fSegmentId);
             fMeta.fSize = size;
         }
+        fLocalPtr = shmPtr.GetUserPtr();
         return fLocalPtr;
     }
 
